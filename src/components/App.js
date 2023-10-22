f1d341d042f5839be8f94b27ceb35be2e7ea935b
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-10-22 18:53:26 -0500

    updated some styles in css

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
+
+
+
+
+
+
+
+
+
 function App() {
   return (
     <div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
-import Stock from "./Stock";
+import React, { useEffect, useState } from "react";
+import Stock from "./Stock"; 
+
 
 function StockContainer() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+      .then((res) => res.json())
+      .then((data) => setStocks(data))
+      .catch((error) => console.log('error', error));
+
+  }, []);
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock) => (
+        <Stock key={stock.id} stock={stock} />
+      ))}
     </div>
   );
 }
